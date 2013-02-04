99031655528b4baf4090729276c08f1b02d21cb2
Enrico Marino <enrico.marino@email.com>
2013-02-05 00:19:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-
 /**
  * Emitter
+ * Event emitter component
+ * 
+ * @copyright 2013 Enrico Marino and Federico Spini
+ * @license MIT
  */
+
+/**
+ * Expose `Emitter`
+ */
+ 
+module.exports = Emitter;
+
+/**
+ * @constructor Emitter
+ */
+
+function Emitter () {
+  this.listeners = [];
+}
