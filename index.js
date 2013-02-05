996473ecb66bb38cc6e9d27cb0e622552490babd
Enrico Marino <enrico.marino@email.com>
2013-02-05 10:26:48 +0100

    Add Emitter#off

--- index.js
+++ index.js
@@ -36,3 +36,37 @@ Emitter.prototype.on = function (event, fn) {
     .push(fn);
   return this;
 };
+
+/**
+ * @method off
+ * @description
+ *   Remove the given callback for `event` 
+ *   or all registered callbacks.
+ *
+ * @param {String} event event
+ * @param {Function} fn callback to remove
+ * @return {Emitter}
+ * @api public
+ */
+
+Emitter.prototype.off = function (event, fn) {
+  var listeners = this._listeners[event];
+  var listener;
+  var i;
+  
+  if (!listener) {
+   return this;
+  }
+
+  if (1 == arguments.length) {
+    delete this._listeners[event];
+    return this;
+  }
+
+  i = listeners.indexOf(fn);
+  if (i !== -1) {
+   callbacks.splice(i, 1);
+  }
+  
+  return this;
+};
